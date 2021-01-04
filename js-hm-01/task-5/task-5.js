f87d96566f7c393720d5fadb08ab0315ccf56731
Valdemar <kalyuzhnyy.volodia@gmail.com>
2021-01-04 18:08:50 +0200

    fix

--- js-hm-01/task-5/task-5.js
+++ js-hm-01/task-5/task-5.js
@@ -2,19 +2,19 @@ let country = prompt('Введите вашу страну').toLowerCase();
 
 switch (country) {
     case 'китай':
-        alert(`Доставка в ${country} будет стоить 150 кредитов`);
+        alert(`Доставка в ${country[0].toUpperCase() + country.slice(1)} будет стоить 150 кредитов`);
         break;
     case 'чили':
-        alert(`Доставка в ${country} будет стоить 250 кредитов`);
+        alert(`Доставка в ${country[0].toUpperCase() + country.slice(1)} будет стоить 250 кредитов`);
         break;
     case 'австралия':
-        alert(`Доставка в ${country} будет стоить 165 кредитов`);
+        alert(`Доставка в ${country[0].toUpperCase() + country.slice(1)} будет стоить 165 кредитов`);
         break;
     case 'индия':
-        alert(`Доставка в ${country} будет стоить 90 кредитов`);
+        alert(`Доставка в ${country[0].toUpperCase() + country.slice(1)} будет стоить 90 кредитов`);
         break;
     case 'ямайка':
-        alert(`Доставка в ${country} будет стоить 130 кредитов`);
+        alert(`Доставка в ${country[0].toUpperCase() + country.slice(1)} будет стоить 130 кредитов`);
         break;
     default:
         alert('В вашей стране доставка не доступна');
