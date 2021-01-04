3a2607b87e24f09df1ea781ff40b3813c104b9c1
Valdemar <kalyuzhnyy.volodia@gmail.com>
2021-01-04 22:12:53 +0200

    fix_task-6

--- js-hm-01/task-6/task-6.js
+++ js-hm-01/task-6/task-6.js
@@ -2,13 +2,27 @@
 let input;
 let total = 0;
 
-do {
+
+// do {
+//     input = prompt("Введите число");
+//     if (input === '' || isNaN(input)) {
+//         alert('Вы ввели не число!')
+//     } else {
+//         total = total + Number(input);
+//     }
+// } while (input);
+
+// alert(`Общая сумма чисел равна ${total}`);
+
+while (true) {
     input = prompt("Введите число");
     if (input === '' || isNaN(input)) {
         alert('Вы ввели не число!')
     } else {
         total = total + Number(input);
     }
-} while (input);
+    if (input == null) break
+}
 
 alert(`Общая сумма чисел равна ${total}`);
+
